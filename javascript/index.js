08211c4f9a69934d3ca8b28876394c0c4d256c88
Charlie Yin <charlieyinjunhao@hotmail.com>
2016-12-24 02:58:01 -0800

    Implemented a way to split progress according to given range length

--- javascript/index.js
+++ javascript/index.js
@@ -5,21 +5,23 @@
 	/*
 	 * Color Class
 	 * Stores the RGB values for this color and provides useful methods for accessing and setting colors
-	 * @param[r]: red value in decimal
-	 * @param[g]: green value in decimal
-	 * @param[b]: blue value in decimal
+	 * @param [r]: red value in decimal
+	 * @param [g]: green value in decimal
+	 * @param [b]: blue value in decimal
 	 */
 	var Color = function(r, g, b) {
 
 		// RGB values for this color (defaults to 0)
-		var m_rgb = {r: r || 0,
-					 g: g || 0,
-					 b: b || 0};
+		var m_rgb = {
+			r: r || 0,
+			g: g || 0,
+			b: b || 0
+		};
 
 		/*
 		 * getRGB()
 		 * Gets the specified rgb values in the input field
-		 * @param[rgb]: An array which specifies which rgb values to return
+		 * @param [rgb]: An array which specifies which rgb values to return
 		 * @return: The specified RGB values
 		 */
 		this.getRGB = function(rgb) {
@@ -51,7 +53,7 @@
 		/*
 		 * setRGB()
 		 * Sets the stored RGB values
-		 * @param[rgb]: An array containing the rgb values in the format [r, g, b]
+		 * @param [rgb]: An array containing the rgb values in the format [r, g, b]
 		 */
 		this.setRGB = function(rgb) {
 
@@ -85,20 +87,13 @@
 		};
 	};
 
-	// Constant colors for different progression points in the format [r, g, b]
-	var progress = {start: [0, 0, 0], mid: [255, 255, 0], end: [255, 255, 255]};
-
-	// Health display and charge display color objects
-	var health_color = new Color(progress.start[0], progress.start[1], progress.start[2]);
-	var charge_color = new Color(progress.start[0], progress.start[1], progress.start[2]);
-
 	/*
 	 * Progress Bar Class
 	 * Creates progress bar objects which has color and progress information
-	 * @param[color]: A color object
-	 * @param[range]: A range of RGB values that define the progress bar color at different points
-	 * @param[start]: Starting progress value
-	 * @param[end]: Ending progress value
+	 * @param [color]: A color object
+	 * @param [range]: A range of RGB values that define the progress bar color at different points
+	 * @param [start]: Starting progress value
+	 * @param [end]: Ending progress value
 	 */
 	var Progress = function(color, range, start, end) {
 
@@ -107,13 +102,14 @@
 		if (range.length < 1) throw new Error("The range must have at least 1 RGB element");
 		if ((start !== null && end !== null) && (start >= end)) throw new Error("Start must be less than end");
 
-		// Private variables
-		var color = color;
-		var range = range;
+		// Color object and color ranges
+		var m_color = color;
+		var m_range = range;
 
-		// Progress end points
-		var start = start || 0;
-		var end = end || 100;
+		// Progress end points and initializes the current progress
+		var m_start = start || 0;
+		var m_end = end || 100;
+		var m_progress = m_start;
 
 		/*
 		 * getBreakpoints()
@@ -123,21 +119,72 @@
 		var getBreakpoints = function() {
 
 			var breakpoints = [];
-			var step = (end - start) / range.length;
+			var length = m_range.length - 1;
+
+			// If the color range array only had 1 element in it, return the start and end points
+			if (length === 0) return [m_start, m_end];
+
+			var step = (m_end - m_start) / length;
 
 			// Add in the breakpoints according to given range
-			for (var val = start + step; val <= end; val = val + step) {
+			for (var val = m_start; val < m_end; val = val + step) {
 				breakpoints.push(val);
+			}
+
+			// Correct errors from comparing floating point and ensuring last element is the end number
+			if (breakpoints.length < length) {
+				breakpoints.push(m_end);
+			} else {
+				breakpoints[length] = m_end;
 			}
 
 			return breakpoints;
 		};
 
-		// Breakpoints array which will break the range into |range.length| segments
-		var breakpoints = getBreakpoints();
+		// Breakpoints array which will break the range into |m_range.length - 1| segments
+		var m_breakpoints = getBreakpoints();
 
-		console.log(breakpoints);
+		/*
+		 * getCurrentRange()
+		 * Helper function to determine the current progress range we're in
+		 * @return: A 2-element array containing the indices of the range in the format [range_start, range_end]
+		 */
+		var getCurrentRange = function() {
+
+			var range_start, range_end;
+
+			for (var i = 1; i < m_breakpoints.length; i++) {
+				if (m_progress <= m_breakpoints[i]) {
+
+					// Check which range the current progress is in and return it
+					range_start = i - 1;
+					range_end = i;
+
+					return [range_start, range_end];
+				}
+			}
+		};
+
+		/*
+		 * setProgress()
+		 * Sets the current stored progress and adjusts the RGB value accordingly
+		 * @param [progress]: The progress to be assigned
+		 */
+		this.setProgress = function(progress) {
+
+			// Check that the progress is within the range
+			if (progress < m_start || progress > m_end) throw new Error("Progress is not within the set range");
+
+			// Sets the current progress
+			m_progress = progress;
+
+			var range = getCurrentRange();
+
+			console.log(m_breakpoints);
+			console.log(m_breakpoints[range[0]], m_breakpoints[range[1]]);
+		};
 	};
 
-	var prog = Progress(health_color, [2]);
+	(new Progress(new Color(), [2, 4, 5, 6], 70.001, 70.1)).setProgress(70.001);
+
 })();
